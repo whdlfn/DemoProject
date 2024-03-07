5599d1d036a5e68ed271b56ff51f81dec5056286
kimsohee <koreait.sec2020@gmail.com>
2024-03-07 16:21:32 +0900

    u #4

--- src/main/java/org/iclass/controller/RequestControllerMapping.java
+++ src/main/java/org/iclass/controller/RequestControllerMapping.java
@@ -3,16 +3,24 @@ package org.iclass.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.iclass.controller.community.ListController;
+import org.iclass.controller.community.ReadController;
+import org.iclass.controller.community.WriteController;
+import org.iclass.controller.community.WriteViewController;
+import org.iclass.controller.member.LoginActionController;
+import org.iclass.controller.member.LoginViewController;
+import org.iclass.controller.member.LogoutController;
+
 public class RequestControllerMapping {
 	private static final Map<RequestKeyValue,Controller> mapping = new HashMap<>();
 	
 	public static void init() {
 		//설계된 url,method 에 따라 처리할 controller 구현체를 Key,Value 로 HashMap에 저장합니다.
-//		mapping.put(new RequestKeyValue("/community/list", "GET"), new ListController() );
-//		mapping.put(new RequestKeyValue("/community/write","GET"), new WriteViewController() );
-//		mapping.put(new RequestKeyValue("/community/write","POST"), new WriteController() );
+		mapping.put(new RequestKeyValue("/community/list", "GET"), new ListController() );
+		mapping.put(new RequestKeyValue("/community/write","GET"), new WriteViewController() );
+		mapping.put(new RequestKeyValue("/community/write","POST"), new WriteController() );
 //		//게시판 글 상세보기 ,수정하기,삭제하기
-//		mapping.put(new RequestKeyValue("/community/read", "GET"), new ReadController() );
+		mapping.put(new RequestKeyValue("/community/read", "GET"), new ReadController() );
 //		mapping.put(new RequestKeyValue("/community/update","GET"), new UpdateViewController() );
 //		mapping.put(new RequestKeyValue("/community/update","POST"), new UpdateController() );
 //		mapping.put(new RequestKeyValue("/community/delete","GET"), new DeleteController() );		//오류 처리 위해 POST로 변경
@@ -28,21 +36,22 @@ public class RequestControllerMapping {
 //		mapping.put(new RequestKeyValue("/member/drop","GET"),new MemberDropController());
 //		
 //		//로그인
-//		mapping.put(new RequestKeyValue("/login", "GET"), new LoginViewController());
-//		mapping.put(new RequestKeyValue("/login", "POST"), new LoginActionController());
-//		mapping.put(new RequestKeyValue("/logout", "GET"), new LogoutController());
-//		//새로나온책 (파일업로드)
+		mapping.put(new RequestKeyValue("/login", "GET"), new LoginViewController());
+		mapping.put(new RequestKeyValue("/login", "POST"), new LoginActionController());
+		mapping.put(new RequestKeyValue("/logout", "GET"), new LogoutController());
+
+		//새로나온책 (파일업로드)
 //		mapping.put(new RequestKeyValue("/book/new", "GET"), new NewFormController());
 //		mapping.put(new RequestKeyValue("/book/new", "POST"), new NewBookSaveController());
 //		mapping.put(new RequestKeyValue("/book/upload", "POST"), new ApiNewBookImageUploadController());
 //		mapping.put(new RequestKeyValue("/book/list", "GET"), new NewBookListController());
-//		
-//		//책장
+
+		//책장
 //		mapping.put(new RequestKeyValue("/book/bookcase","GET"), new BookCaseListController());
 //		mapping.put(new RequestKeyValue("/pay/success","GET"), new BookCasePayRequestSuccessController());
 //		mapping.put(new RequestKeyValue("/pay/fail","GET"), new BookCasePayRequestFailController());
-//		
-//		//공지사항(검색기능)
+
+		//공지사항(검색기능)
 //		mapping.put(new RequestKeyValue("/notice/list", "GET"), new NoticeListController());
 //		mapping.put(new RequestKeyValue("/notice/read", "GET"), new NoticeReadController());
 //		mapping.put(new RequestKeyValue("/notice/write", "GET"), new NoticeWriteController());
@@ -51,8 +60,8 @@ public class RequestControllerMapping {
 //		mapping.put(new RequestKeyValue("/notice/update", "POST"), new NoticeUpdateSaveController());
 //		mapping.put(new RequestKeyValue("/notice/delete", "GET"), new NoticeDeleteController());
 //		mapping.put(new RequestKeyValue("/notice/download", "POST"), new NoticeFileDownloadController());
-//		
-//		// api mapping
+
+		// api mapping
 //		mapping.put(new RequestKeyValue("/api/auth/checkId", "POST"), new ApiCheckIdController() );
 //		mapping.put(new RequestKeyValue("/api/auth/join", "POST"), new ApiMemberJoinController() );
 //		mapping.put(new RequestKeyValue("/api/book", "GET"), new ApiGetBookOneController() );
